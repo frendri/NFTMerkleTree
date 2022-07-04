346889b3aaf153576c8eb20a7c1b91d5a691241a
tern <dan.ternovoi@gmail.com>
2022-07-04 20:47:12 +0400

    sha -> keccak

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import {MerkleTree} from "merkletreejs";
-const SHA256 = require('crypto-js/sha256')
+const keccak256 = require('keccak256')
 const fs = require('fs')
 
 function getProofForAddress(address: string, merkleTree: MerkleTree) : string[] {
-    return merkleTree.getHexProof(address)
+    return merkleTree.getHexProof(keccak256(address))
 }
 
-const exampleProofAddress = '0x496fd840eFfcb08065be99878E45A466323eaFd5'
+const exampleProofAddress = '0x8ce8e2B8277796367D68fDd2eC3E95b942C19341'
 
 const wlAddresses = fs.readFileSync('wlAddresses.txt', {encoding:'utf-8'}).split('\n')
-const leaves = wlAddresses.map((wlAddress: string) => SHA256(wlAddress))
+const leaves = wlAddresses.map((wlAddress: string) => keccak256(wlAddress))
 
-const tree = new MerkleTree(leaves, SHA256)
+const tree = new MerkleTree(leaves, keccak256, {sort: true})
 
 const root = tree.getRoot().toString('hex')
 
 console.log('Your merkle root:', root)
 
-const proof = getProofForAddress(SHA256(exampleProofAddress), tree)
+const proof = getProofForAddress(exampleProofAddress, tree)
 console.log('Merkle proof for address:', proof)
 
 
